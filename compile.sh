496c82475793c1491177f256dbf15471a8a18046
Bao Vuong <vqbao@lv5.org>
2015-10-24 02:16:54 -0400

    fixed compile script

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 
 
 
-if [$# > 0]; then
-    clang $@ -I `gnustep-config --variable=GNUSTEP_SYSTEM_HEADERS` -L `gnustep-config --variable=GNUSTEP_SYSTEM_LIBRARIES` -lgnustep-base -fconstant-string-class=NSConstantString -D_NATIVE_OBJC_EXCEPTIONS -lobj
-else
+if [[ $# = "0" ]]; then
     echo "no input"
+else
+    clang $@ -I `gnustep-config --variable=GNUSTEP_SYSTEM_HEADERS` -L `gnustep-config --variable=GNUSTEP_SYSTEM_LIBRARIES` -lgnustep-base -fconstant-string-class=NSConstantString -D_NATIVE_OBJC_EXCEPTIONS -lobjc
 fi
